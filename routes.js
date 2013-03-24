3572bd394c532a2850cec1a371003941275c064e
Juho Vepsalainen <bebraw@gmail.com>
2013-03-24 21:48:07 +0200

    Add a demo table

--- routes.js
+++ routes.js
@@ -1,3 +1,23 @@
 exports.index = function(req, res) {
-    res.render('index', {});
+    res.render('index', {
+        types: ['CDN', 'Uptime', 'Latency', 'Projects', 'Provider'],
+        cdns: [
+            {
+                name: 'cdnjs',
+                link: 'http://cdnjs.com/',
+                uptime: 99.9,
+                latency: [],
+                projects: 123,
+                provider: 'TODO'
+            },
+            {
+                name: 'jsDelivr',
+                link: 'http://www.jsdelivr.com/',
+                uptime: 99.9,
+                latency: [],
+                projects: 123,
+                provider: 'TODO'
+            }
+        ]
+    });
 };
