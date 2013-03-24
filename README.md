253180304181a289ed2a27873638273387bec0b9
Juho Vepsalainen <bebraw@gmail.com>
2013-03-24 21:21:33 +0200

    Set up initial project

---
+++ README.md
@@ -0,0 +1,8 @@
+# CDNperf
+
+CDNperf monitors popular CDN sites and graphs this data.
+
+## Development
+
+1. `sudo npm install`
+2. `cp example.config.js config.js` and adjust as needed
---
+++ serve.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+var http = require('http');
+var path = require('path');
+
+var express = require('express');
+
+var routes = require('./routes');
+var config = require('./config');
+
+
+main();
+
+function main() {
+    var app = express();
+
+    app.configure(function() {
+        app.set('port', config.port);
+
+        app.set('views', __dirname + '/views');
+        app.set('view engine', 'jade');
+
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express['static'](path.join(__dirname, 'public')));
+
+        app.use(express.cookieParser(config.cookieSecret));
+        app.use(express.session());
+
+        app.use(app.router);
+    });
+
+    app.configure('development', function() {
+        app.use(express.errorHandler());
+    });
+
+    app.get('/', routes.index);
+
+    http.createServer(app).listen(config.port, function() {
+        console.log('Server running at ' + config.port);
+    });
+}
