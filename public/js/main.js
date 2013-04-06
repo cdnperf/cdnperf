9b34f619b93e7e8fb60671e8f05c59ece2d98adb
Juho Vepsalainen <bebraw@gmail.com>
2013-04-06 12:12:37 +0300

    Render a chart for each provider

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,32 @@
 $(main);
 
 function main() {
-    $.getJSON('./data.json', function(data) {
+    $.getJSON('./data.json', function(providers) {
+        var $p = $('.latencies');
+
+        // TODO: add shim for older browsers
+        providers.forEach(function(provider) {
+            createChart($p, provider);
+        });
+    });
+
+    function createChart($p, provider) {
         var d = {
             xScale: 'time',
             yScale: 'linear',
             type: 'line',
             main: [{
                 className: 'latency',
-                data: data[0].data
+                data: provider.data
             }]
         };
+        // TODO: move to a template
+        var $l = $('<div>', {'class': 'latency'}).appendTo($p);
+        $('<div>', {'class': 'name'}).text(provider.name).appendTo($l);
+        $('<div>', {'class': 'host'}).text(provider.host).appendTo($l);
+        $('<div>', {'class': 'latency'}).text(provider.lastresponse + 'ms').appendTo($l);
+        var $e = $('<figure>', {'class': 'chart'}).appendTo($l);
 
-        new xChart('latency', d, '#myChart');
-    });
+        new xChart('latency', d, $e.get(0));
+    }
 }
