9008317a9a089c17ce1b4704f50e5a3b1a9cc86a
Juho Vepsalainen <bebraw@gmail.com>
2013-05-23 16:17:15 +0300

    Add some openshift specific tweaks

--- serve.js
+++ serve.js
@@ -40,7 +40,30 @@ function main() {
 
     cronjobs();
 
-    http.createServer(app).listen(config.port, function() {
-        console.log('Server running at ' + config.port);
+    // openshift tweaks
+    var ipaddr = process.env.OPENSHIFT_INTERNAL_IP;
+    var port = process.env.OPENSHIFT_INTERNAL_PORT || config.port;
+
+    process.on('exit', terminator);
+
+    ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT', 'SIGBUS',
+    'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGPIPE', 'SIGTERM'
+    ].forEach(function(element, index, array) {
+        process.on(element, function() { terminator(element); });
     });
+
+    app.listen(port, ipaddr, function() {
+        console.log('Server running at ' + port);
+    });
+}
+
+function terminator(sig) {
+    if(typeof sig === "string") {
+        console.log('%s: Received %s - terminating Node server ...',
+            Date(Date.now()), sig);
+
+        process.exit(1);
+    }
+
+    console.log('%s: Node server stopped.', Date(Date.now()) );
 }
