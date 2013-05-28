5f76c3240348c6a7f722a9319931dc102ff91fed
Juho Vepsalainen <bebraw@gmail.com>
2013-05-28 15:19:44 +0300

    Use pingdom summary API to fetch latency and uptime data
    This is very effective request-wise. It costs around 1 + <amount of
    checks> to get the latency and uptime info.
    
    Currently this is performed once per day using cron although that can be
    changed if needed.
    
    Note that the frontend still needs to be updated to use the new data
    format. It is more compact than the previous one. There still could be
    some problems in it, though.

--- cronjobs.js
+++ cronjobs.js
@@ -1,45 +1,107 @@
 var fs = require('fs');
 
-var async = require('async');
-var cronJob = require('cron').CronJob;
+require('date-utils');
 
 var config = require('./config');
-var data = require('./data')(config.pingdom);
+
+var async = require('async');
+var cronJob = require('cron').CronJob;
+var pingdom = require('pingdom-api')(config.pingdom);
 
 
 function init() {
-    writeJSON('data.json');
+    writeJSON();
+
+    new cronJob('*/ 0 0 0 * * *', writeJSON, null, true);
 }
 module.exports = init;
 
-function writeJSON(outputName) {
-    if(!config.amountOfDays) return console.error('Missing "amountOfDays" at config');
+function writeJSON() {
+    pingdom.checks(function(err, checks) {
+        if(err) return console.error(err);
 
-    var params = {
-        range: config.amountOfDays,
-        date: new Date()
+        async.parallel(forAll(checks, getSummary), function(err, data) {
+            if(err) return console.error(err);
+
+            write(JSON.stringify(structure(data)), './public/data.json');
+        });
+    });
+}
+
+function forAll(checks, fn) {
+    return checks.map(function(check) {
+        return function(cb) {
+            fn(check, cb);
+        };
+    });
+}
+
+function getSummary(check, cb) {
+    var to = Date.today();
+    var from = to.clone().addMonths(-6);
+
+    pingdom['summary.performance'](function(err, data) {
+        cb(err, {
+            check: check,
+            data: data
+        }); // skip res
+    }, {
+        target: check.id,
+        qs: {
+            from: from,
+            to: to,
+            resolution: 'day',
+            includeuptime: true
+        }
+    });
+}
+
+function structure(data) {
+    return {
+        meta: parseMeta(data),
+        latency: parseLatency(data),
+        uptime: parseUptime(data)
     };
 
-    write();
+    function parseMeta(data) {
+        var days = data[0].data.days;
 
-    new cronJob('*/ 0 0 * * *', write, null, true);
+        return {
+            providers: data.map(function(d) {
+                var check = d.check;
 
-    function write() {
-        async.series([
-            data.latency.bind(undefined, params),
-            data.uptime.bind(undefined, params)
-        ], function(err, data) {
-            if(err) return console.error(err);
-            if(data.length != 2) return console.error('writeJSON - received wrong amount of data!');
+                return {
+                    name: check.name,
+                    host: check.hostname,
+                    type: check.name.split(' ')[1].toLowerCase()
+                };
+            }),
+            firstDay: days[0].starttime,
+            lastDay: days[days.length - 1].starttime
+        };
+    }
 
-            fs.writeFile('./public/' + outputName, JSON.stringify({
-                latency: data[0],
-                uptime: data[1]
-            }), function(err) {
-                if(err) return console.error(err);
-
-                console.log('Wrote ' + outputName);
+    function parseLatency(data) {
+        return data.map(function(d) {
+            return d.data.days.map(function(v) {
+                return v.avgresponse;
             });
         });
     }
+
+    function parseUptime(data) {
+        return data.map(function(d) {
+            return d.data.days.map(function(v) {
+                return (v.uptime / (v.uptime + v.downtime)).toFixed(4);
+            });
+        });
+    }
+}
+
+function write(data, target) {
+    fs.writeFile(target, data, function(err) {
+        if(err) return console.error(err);
+
+        console.log('Wrote ' + target);
+    });
 }
