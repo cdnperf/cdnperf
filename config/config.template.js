a66eb931e133cb708a0aa70e76f820b437eaaf32
Juho Vepsalainen <bebraw@gmail.com>
2013-07-05 16:26:09 +0300

    Use empty expression for cron by default
    This way it won't fail (nor execute).

--- config/config.template.js
+++ config/config.template.js
@@ -2,7 +2,7 @@ module.exports = {
     port: 8000,
     ga: 'UA-XXXXX-X',
     bugira: 'replacethis',
-    cron: 'some expression',
+    cron: '',
     pingdom: {
         user: 'replacethis',
         pass: 'replacethis',
