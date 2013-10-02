28a16b70f1ef8642086281582f07307b2016058a
Juho Vepsalainen <bebraw@gmail.com>
2013-10-02 15:29:50 +0300

    Remove unnecessary middlewares

--- serve.js
+++ serve.js
@@ -23,8 +23,6 @@ function main() {
 
         app.use(express.favicon('public/images/favicon.ico'));
         app.use(express.logger('dev'));
-        app.use(express.bodyParser());
-        app.use(express.methodOverride());
         app.use(express.compress());
         app.use(express['static'](path.join(__dirname, 'public'), {
             maxAge: halfDay
