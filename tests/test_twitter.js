a3a0d561306fc891f054eb1ea03ef69e879f89d8
Juho Vepsalainen <bebraw@gmail.com>
2013-11-10 14:52:07 +0200

    Improve structure

--- tests/test_twitter.js
+++ tests/test_twitter.js
@@ -1,5 +1,5 @@
-var config = require('./config').twitter;
-var twitter = require('./twitter')(config);
+var config = require('../config').twitter;
+var twitter = require('../lib/twitter')(config);
 
 twitter.get('statuses/user_timeline', function(err, data) {
     if(err) return console.error(err);
