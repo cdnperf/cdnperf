08be029fc2c56331a2c9b2d38431fbe98f16ba1c
Juho Vepsalainen <bebraw@gmail.com>
2014-01-06 18:18:52 +0200

    Add initial files generated by Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('cdnperfApp', [
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
