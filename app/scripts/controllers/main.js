b7375c306fbdac0d5cea5cbf587b1bbf8a0c2b1b
Juho Vepsalainen <bebraw@gmail.com>
2014-01-07 14:11:50 +0200

    Sketch out main view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
-angular.module('cdnperfApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('cdnperfApp').controller('MainCtrl', function($scope) {
+    // TODO
+    $scope.demo = 'foo';
+});
