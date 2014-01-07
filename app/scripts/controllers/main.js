ca8ac4fddb0a64ef8e698451705ef0ca88d35328
Juho Vepsalainen <bebraw@gmail.com>
2014-01-07 14:21:52 +0200

    Fetch data

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,19 @@
 'use strict';
 
-angular.module('cdnperfApp').controller('MainCtrl', function($scope) {
-    // TODO
-    $scope.demo = 'foo';
+angular.module('cdnperfApp').controller('MainCtrl', function($scope, $http) {
+    $scope.providers = [];
+
+    $http.get('data/data.json').then(function(res) {
+        $scope.providers = res.data.providers;
+        // TODO: pick firstDate and lastDate too
+
+        console.log(res.data);
+    });
+
+    /*
+    $(function() {
+        var val = localStorage.getItem('index');
+        if(val) $('.indexUrl').attr('href', val);
+    });
+    */
 });
