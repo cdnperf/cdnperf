4d2241eb0154c6528e8ab650496c42851949366e
Juho Vepsalainen <bebraw@gmail.com>
2014-01-08 12:35:23 +0200

    Set up a demo chart

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,36 @@
 
 angular.module('cdnperfApp').controller('MainCtrl', function($scope, $http) {
     $scope.providers = [];
+    $scope.uptimes = {
+        series: ['Sales', 'Income', 'Expense', 'Laptops', 'Keyboards'],
+        data : [
+            {
+                x: 'Sales',
+                y: [100,500, 0],
+                tooltip: 'this is tooltip'
+            },
+            {
+                x: 'Not Sales',
+                y: [300, 100, 100]
+            },
+            {
+                x: 'Tax',
+                y: [351]
+            },
+            {
+                x: 'Not Tax',
+                y: [54, 0, 879]
+            }
+        ]
+    };
+    $scope.chartConfig = {
+        labels: false,
+        title : 'Not Products',
+        legend : {
+            display: true,
+            position: 'left'
+        }
+    };
 
     $http.get('data/data.json').then(function(res) {
         $scope.providers = res.data.providers;
