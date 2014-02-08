399b762ba96ed4e89f473213438640f9fc239fd5
Juho Vepsalainen <bebraw@gmail.com>
2014-02-08 21:43:48 +0100

    Eliminate redundant callback
    No need for that after all.

--- tasks/update_data.js
+++ tasks/update_data.js
@@ -24,12 +24,6 @@ module.exports = function(cb) {
             write(JSON.stringify(d), './public/data.json');
 
             cb(null, d);
-        }, function(err) {
-            if(err) {
-                return cb(err);
-            }
-
-            console.log('Updated data.json');
         });
     });
 };
