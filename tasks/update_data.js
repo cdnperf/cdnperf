e903df0a3db75595da1af918e97ab75aceb57b27
Juho Vepsalainen <bebraw@gmail.com>
2015-05-08 15:43:36 +0300

    Add initial dotcom data fetcher
    Still need to calculate average for latencies + check output.

--- tasks/update_data.js
+++ tasks/update_data.js
@@ -10,34 +10,121 @@ require('date-utils');
 
 
 module.exports = function(cb) {
+    var to = Date.today();
+    var from = to.clone().addMonths(-3);
+
+    async.parallel([
+        getDotcomData.bind(null, {
+            from: from,
+            to: to,
+            auth: config.dotcom.auth
+        }),
+        getPingdomData.bind(null, {
+            from: from,
+            to: to
+        }),
+    ], function(err, data) {
+        if(err) {
+            return cb(err);
+        }
+
+        // TODO: merge data now
+        // go through latency + avg if not null
+
+        var p = path.join(__dirname, '../public/data.json');
+
+        writeData(data[1], p, cb);
+    });
+};
+
+function writeData(d, target, cb) {
+    write(JSON.stringify(d), target, function(err) {
+        if(err) {
+            return cb(err);
+        }
+
+        cb(null, d);
+    });
+}
+
+/* dotcom */
+function getDotcomData(o, cb) {
+    var fmt = 'MM/DD/YYYY';
+    var url = 'https://xmlreporter.dotcom-monitor.com/reporting/xml/responses.aspx?' +
+        qs.stringify({
+            pid: o.auth,
+            Site: '*',
+            Type: 'Day',
+            Options: 'AllDownPercentages',
+            From: o.from.toFormat(fmt),
+            To: o.to.toFormat(fmt),
+        });
+
+    request(url, function(err, res) {
+        if(err) {
+            return cb(err);
+        }
+
+        var xml = res.body;
+
+        parseString(xml, function(err, result) {
+            if(err) {
+                return cb(err);
+            }
+
+            var siteData = result.DotcomMonitorOnlineReport.Site;
+
+            cb(null, {
+                providers: parseDotcomAverages(siteData)
+            });
+        });
+    });
+}
+
+function parseDotcomAverages(siteData) {
+    return siteData.map(function(v) {
+        var attrs = v.$;
+        var summary = v.Summary;
+
+        return {
+            name: attrs.Name,
+            latency: summary.map(function(s) {
+                var avgRes = s['Average-Response-Time'];
+                var avgResponse = avgRes ? avgRes[0] : null;
+                avgResponse = parseFloat(avgResponse);
+
+                if(!avgResponse) {
+                    avgResponse = null;
+                }
+
+                return avgResponse;
+            })
+        };
+    });
+}
+
+/* pingdom */
+function getPingdomData(o, cb) {
     pingdom.checks(function(err, checks) {
         if(err) {
             return cb(err);
         }
 
-        async.parallel(constructChecks(checks), function(err, data) {
+        async.parallel(constructChecks(o, checks), function(err, data) {
             if(err) {
                 return cb(err);
             }
 
-            var d = structure(data);
-
-            write(JSON.stringify(d), path.join(__dirname, '../public/data.json'), function(err) {
-                if(err) {
-                    return cb(err);
-                }
-
-                cb(null, d);
-            });
+            cb(null, structure(data));
         });
     });
-};
+}
 
-function constructChecks(checks) {
+function constructChecks(o, checks) {
     return checks.map(function(check) {
         return function(cb) {
-            var to = Date.today();
-            var from = to.clone().addMonths(-3);
+            var from = o.from;
+            var to = o.to;
 
             async.series([
                 getSummaries.bind(undefined, check, from, to),
